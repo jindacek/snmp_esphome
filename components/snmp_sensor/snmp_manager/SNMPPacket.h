95415c90dee95a83d21499b135d64ea31afb8e83
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-18 18:38:35 +0100

    Update SNMPPacket.h

--- components/snmp_sensor/snmp_manager/SNMPPacket.h
+++ components/snmp_sensor/snmp_manager/SNMPPacket.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <Arduino.h>
+#include "SNMPVariable.h"
+
+class SNMPPacket {
+public:
+  SNMPPacket();
+
+  void setVersion(int version);
+  void setCommunity(const char* community);
+  void addGetRequest(const char* oid);
+
+  uint8_t* data();
+  int length();
+
+  static bool parseResponse(uint8_t* data, int len, SNMPVariable* out);
+
+private:
+  uint8_t buffer_[512];
+  int length_;
+};
 
