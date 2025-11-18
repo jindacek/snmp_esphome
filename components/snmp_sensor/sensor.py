c15695ce47aa68c83fc3b76945f865f3932ca06c
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-18 16:55:55 +0100

    Update sensor.py

--- components/snmp_sensor/sensor.py
+++ components/snmp_sensor/sensor.py
@@ -1,1 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import CONF_ID
+from . import snmp_ns
 
+SnmpSensor = snmp_ns.class_("SnmpSensor", sensor.Sensor, cg.PollingComponent)
+
+CONFIG_SCHEMA = (
+    sensor.sensor_schema()
+    .extend({
+        cv.GenerateID(): cv.declare_id(SnmpSensor),
+    })
+    .extend(cv.polling_component_schema("10s"))
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await sensor.register_sensor(var, config)
+    await cg.register_component(var, config)
