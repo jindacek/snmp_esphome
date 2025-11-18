3b07e099342c1b3b7d14627769182606e7a1e873
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-18 18:40:43 +0100

    Update snmp_sensor.h

--- components/snmp_sensor/snmp_sensor.h
+++ components/snmp_sensor/snmp_sensor.h
@@ -1,19 +1,19 @@
 #pragma once
 
-#include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace snmp_sensor {
 
-class SnmpSensor : public sensor::Sensor, public PollingComponent {
+class SnmpSensor : public sensor::Sensor, public Component {
  public:
+  void setup() override;
+  void update() override;
+
   void set_host(const std::string &host) { host_ = host; }
   void set_community(const std::string &community) { community_ = community; }
   void set_oid(const std::string &oid) { oid_ = oid; }
-
-  void setup() override;
-  void update() override;
 
  protected:
   std::string host_;
