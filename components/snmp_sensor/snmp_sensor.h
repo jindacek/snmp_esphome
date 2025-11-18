9c56fa596027db7c6508b4bb061b49c95a76d078
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-18 17:26:28 +0100

    Update snmp_sensor.h

--- components/snmp_sensor/snmp_sensor.h
+++ components/snmp_sensor/snmp_sensor.h
@@ -8,7 +8,16 @@ namespace snmp_sensor {
 
 class SnmpSensor : public sensor::Sensor, public PollingComponent {
  public:
+  void set_host(const std::string &host) { host_ = host; }
+  void set_community(const std::string &community) { community_ = community; }
+  void set_oid(const std::string &oid) { oid_ = oid; }
+
   void update() override;
+
+ protected:
+  std::string host_;
+  std::string community_;
+  std::string oid_;
 };
 
 }  // namespace snmp_sensor
