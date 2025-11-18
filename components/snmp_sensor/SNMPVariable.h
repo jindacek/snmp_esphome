5a7c8904a0706dfab171373575967aba64b9cf67
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-18 18:52:14 +0100

    Create SNMPVariable.h

---
+++ components/snmp_sensor/SNMPVariable.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <Arduino.h>
+
+class SNMPVariable {
+public:
+  SNMPVariable() : type_(0), int_value_(0) {}
+
+  void setInteger(long value) { type_ = 2; int_value_ = value; }
+  long asInteger() { return int_value_; }
+
+private:
+  int type_;
+  long int_value_;
+};
