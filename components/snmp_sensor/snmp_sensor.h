cc52b39195da85c57b122180918e76454766c4f6
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-18 16:56:45 +0100

    Update snmp_sensor.h

--- components/snmp_sensor/snmp_sensor.h
+++ components/snmp_sensor/snmp_sensor.h
@@ -1,1 +1,15 @@
+#pragma once
 
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+
+namespace esphome {
+namespace snmp_sensor {
+
+class SnmpSensor : public sensor::Sensor, public PollingComponent {
+ public:
+  void update() override;
+};
+
+}  // namespace snmp_sensor
+}  // namespace esphome
