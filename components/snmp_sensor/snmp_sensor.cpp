3e359062c967caad3059fb4948808862b1dda1d2
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-18 17:26:44 +0100

    Update snmp_sensor.cpp

--- components/snmp_sensor/snmp_sensor.cpp
+++ components/snmp_sensor/snmp_sensor.cpp
@@ -7,8 +7,12 @@ namespace snmp_sensor {
 static const char *TAG = "snmp_sensor";
 
 void SnmpSensor::update() {
-  ESP_LOGD(TAG, "SnmpSensor update() called");
-  // tady později doplníme SNMP dotaz
+  ESP_LOGD(TAG, "SNMP request: host=%s, community=%s, oid=%s",
+           host_.c_str(), community_.c_str(), oid_.c_str());
+
+  // TODO: implement SNMP GET
+  // zatím jen placeholder
+  this->publish_state(0);
 }
 
 }  // namespace snmp_sensor
