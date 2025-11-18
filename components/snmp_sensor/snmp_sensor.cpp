15db51fcf7cdcc8186d44e27e568756737232ade
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-18 17:47:53 +0100

    Update snmp_sensor.cpp

--- components/snmp_sensor/snmp_sensor.cpp
+++ components/snmp_sensor/snmp_sensor.cpp
@@ -6,13 +6,25 @@ namespace snmp_sensor {
 
 static const char *TAG = "snmp_sensor";
 
+// -----------------------------------------------------
+// Setup – zatím prázdné, jen log
+// -----------------------------------------------------
+void SnmpSensor::setup() {
+  ESP_LOGI(TAG, "SNMP sensor initialized");
+}
+
+// -----------------------------------------------------
+// Update – zde později bude SNMP GET
+// -----------------------------------------------------
 void SnmpSensor::update() {
   ESP_LOGD(TAG, "SNMP request: host=%s, community=%s, oid=%s",
            host_.c_str(), community_.c_str(), oid_.c_str());
 
-  // TODO: implement SNMP GET
-  // zatím jen placeholder
-  this->publish_state(0);
+  // ZATÍM: jen placeholder
+  // Ať to kompiluje, funguje a je připravené.
+  float dummy_value = 0.0f;
+
+  this->publish_state(dummy_value);
 }
 
 }  // namespace snmp_sensor
