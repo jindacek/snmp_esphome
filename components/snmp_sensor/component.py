61b1c8fc979af03674332970df1e118ee780209b
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-19 10:39:34 +0100

    Update component.py

--- components/snmp_sensor/component.py
+++ components/snmp_sensor/component.py
@@ -1,32 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_PORT
 
 DEPENDENCIES = ['network']
 AUTO_LOAD = ['sensor']
 
 snmp_ns = cg.esphome_ns.namespace('snmp')
-SNMPSensor = snmp_ns.class_('SNMPSensor', sensor.Sensor, cg.Component)
-
-CONF_COMMUNITY = 'community'
-CONF_OID = 'oid'
-CONF_HOST = 'host'
+# ZMĚNA: SnmpSensor místo SNMPSensor
+SnmpSensor = snmp_ns.class_('SnmpSensor', sensor.Sensor, cg.Component)
 
 CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(SNMPSensor),
-    cv.Required(CONF_HOST): cv.string,
-    cv.Required(CONF_COMMUNITY): cv.string,
-    cv.Required(CONF_OID): cv.string,
-    cv.Optional(CONF_PORT, default=161): cv.port,
+    cv.GenerateID(): cv.declare_id(SnmpSensor),
+    cv.Required("host"): cv.string,
+    cv.Required("community"): cv.string,
+    cv.Required("oid"): cv.string,
+    cv.Optional("port", default=161): cv.port,
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config["id"])
     yield cg.register_component(var, config)
     yield sensor.register_sensor(var, config)
     
-    cg.add(var.set_host(config[CONF_HOST]))
-    cg.add(var.set_community(config[CONF_COMMUNITY]))
-    cg.add(var.set_oid(config[CONF_OID]))
-    cg.add(var.set_port(config[CONF_PORT]))
+    cg.add(var.set_host(config["host"]))
+    cg.add(var.set_community(config["community"]))
+    cg.add(var.set_oid(config["oid"]))
+    cg.add(var.set_port(config["port"]))
