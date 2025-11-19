e858a5a68a6ec3ea39ea6264781a92e76b6a6990
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-19 15:41:23 +0100

    Update snmp_sensor.h

--- components/snmp_sensor/snmp_sensor.h
+++ components/snmp_sensor/snmp_sensor.h
@@ -1,27 +1,26 @@
 #pragma once
-
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "snmp_client.h"
 
 namespace esphome {
 namespace snmp_sensor {
 
-class SnmpSensor : public PollingComponent, public sensor::Sensor {
+class SnmpSensor : public sensor::Sensor, public PollingComponent {
  public:
-  // Konstruktor musí přijmout interval – ESPHome to vyžaduje
-  SnmpSensor() : PollingComponent(2000) {}   // default 2000 ms
+  void set_host(const std::string &h) { host_ = h; }
+  void set_community(const std::string &c) { community_ = c; }
+  void set_oid(const std::string &o) { oid_ = o; }
 
   void setup() override;
   void update() override;
 
-  void set_host(const std::string &host) { host_ = host; }
-  void set_community(const std::string &community) { community_ = community; }
-  void set_oid(const std::string &oid) { oid_ = oid; }
-
- protected:
+ private:
   std::string host_;
   std::string community_;
   std::string oid_;
+
+  SnmpClient snmp_;     // ← klient jako člen třídy
 };
 
 }  // namespace snmp_sensor
