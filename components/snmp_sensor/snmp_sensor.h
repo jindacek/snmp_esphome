62356450ec67bd84fa79d49eea0820fc1410be97
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-20 12:58:42 +0100

    Update snmp_sensor.h

--- components/snmp_sensor/snmp_sensor.h
+++ components/snmp_sensor/snmp_sensor.h
@@ -12,22 +12,22 @@ class SnmpSensor : public PollingComponent {
   SnmpSensor(const std::string &host, const std::string &community)
       : host_(host), community_(community) {}
 
-  void set_runtime_sensor(sensor::Sensor *s) { runtime_sensor_ = s; }
-  void set_battery_capacity_sensor(sensor::Sensor *s) { battery_capacity_sensor_ = s; }
-  void set_battery_temp_sensor(sensor::Sensor *s) { battery_temp_sensor_ = s; }
-  void set_battery_voltage_sensor(sensor::Sensor *s) { battery_voltage_sensor_ = s; }
-  void set_input_voltage_sensor(sensor::Sensor *s) { input_voltage_sensor_ = s; }
-  void set_output_voltage_sensor(sensor::Sensor *s) { output_voltage_sensor_ = s; }
-  void set_load_sensor(sensor::Sensor *s) { load_sensor_ = s; }
-  void set_output_status_sensor(sensor::Sensor *s) { output_status_sensor_ = s; }
-  void set_model_sensor(sensor::Sensor *s) { model_sensor_ = s; }
-  void set_name_sensor(sensor::Sensor *s) { name_sensor_ = s; }
-  void set_manufacture_date_sensor(sensor::Sensor *s) { manufacture_date_sensor_ = s; }
-  void set_last_battery_replacement_sensor(sensor::Sensor *s) { last_battery_replacement_sensor_ = s; }
-  void set_last_start_time_sensor(sensor::Sensor *s) { last_start_time_sensor_ = s; }
-
   void setup() override;
   void update() override;
+
+  void set_runtime_sensor(sensor::Sensor *s) { this->runtime_sensor_ = s; }
+  void set_battery_capacity_sensor(sensor::Sensor *s) { this->battery_capacity_sensor_ = s; }
+  void set_battery_temp_sensor(sensor::Sensor *s) { this->battery_temp_sensor_ = s; }
+  void set_battery_voltage_sensor(sensor::Sensor *s) { this->battery_voltage_sensor_ = s; }
+  void set_input_voltage_sensor(sensor::Sensor *s) { this->input_voltage_sensor_ = s; }
+  void set_output_voltage_sensor(sensor::Sensor *s) { this->output_voltage_sensor_ = s; }
+  void set_load_sensor(sensor::Sensor *s) { this->load_sensor_ = s; }
+  void set_output_status_sensor(sensor::Sensor *s) { this->output_status_sensor_ = s; }
+  void set_model_sensor(sensor::Sensor *s) { this->model_sensor_ = s; }
+  void set_name_sensor(sensor::Sensor *s) { this->name_sensor_ = s; }
+  void set_manufacture_date_sensor(sensor::Sensor *s) { this->manufacture_date_sensor_ = s; }
+  void set_last_battery_replacement_sensor(sensor::Sensor *s) { this->last_battery_replacement_sensor_ = s; }
+  void set_last_start_time_sensor(sensor::Sensor *s) { this->last_start_time_sensor_ = s; }
 
  protected:
   std::string host_;
