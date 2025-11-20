0cdf43e18ccc07667029c53b4a4b383faea016e1
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-20 12:44:53 +0100

    Update snmp_sensor.h

--- components/snmp_sensor/snmp_sensor.h
+++ components/snmp_sensor/snmp_sensor.h
@@ -12,6 +12,20 @@ class SnmpSensor : public PollingComponent {
   SnmpSensor(const std::string &host, const std::string &community)
       : host_(host), community_(community) {}
 
+  void set_runtime_sensor(sensor::Sensor *s) { runtime_sensor_ = s; }
+  void set_battery_capacity_sensor(sensor::Sensor *s) { battery_capacity_sensor_ = s; }
+  void set_battery_temp_sensor(sensor::Sensor *s) { battery_temp_sensor_ = s; }
+  void set_battery_voltage_sensor(sensor::Sensor *s) { battery_voltage_sensor_ = s; }
+  void set_input_voltage_sensor(sensor::Sensor *s) { input_voltage_sensor_ = s; }
+  void set_output_voltage_sensor(sensor::Sensor *s) { output_voltage_sensor_ = s; }
+  void set_load_sensor(sensor::Sensor *s) { load_sensor_ = s; }
+  void set_output_status_sensor(sensor::Sensor *s) { output_status_sensor_ = s; }
+  void set_model_sensor(sensor::Sensor *s) { model_sensor_ = s; }
+  void set_name_sensor(sensor::Sensor *s) { name_sensor_ = s; }
+  void set_manufacture_date_sensor(sensor::Sensor *s) { manufacture_date_sensor_ = s; }
+  void set_last_battery_replacement_sensor(sensor::Sensor *s) { last_battery_replacement_sensor_ = s; }
+  void set_last_start_time_sensor(sensor::Sensor *s) { last_start_time_sensor_ = s; }
+
   void setup() override;
   void update() override;
 
@@ -19,6 +33,20 @@ class SnmpSensor : public PollingComponent {
   std::string host_;
   std::string community_;
   SnmpClient snmp_;
+
+  sensor::Sensor *runtime_sensor_{nullptr};
+  sensor::Sensor *battery_capacity_sensor_{nullptr};
+  sensor::Sensor *battery_temp_sensor_{nullptr};
+  sensor::Sensor *battery_voltage_sensor_{nullptr};
+  sensor::Sensor *input_voltage_sensor_{nullptr};
+  sensor::Sensor *output_voltage_sensor_{nullptr};
+  sensor::Sensor *load_sensor_{nullptr};
+  sensor::Sensor *output_status_sensor_{nullptr};
+  sensor::Sensor *model_sensor_{nullptr};
+  sensor::Sensor *name_sensor_{nullptr};
+  sensor::Sensor *manufacture_date_sensor_{nullptr};
+  sensor::Sensor *last_battery_replacement_sensor_{nullptr};
+  sensor::Sensor *last_start_time_sensor_{nullptr};
 };
 
 }  // namespace snmp_sensor
