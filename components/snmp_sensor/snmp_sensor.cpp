357874fffe0598c524ac30911e1d22ccb448b4cc
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-20 16:20:21 +0100

    Update snmp_sensor.cpp

--- components/snmp_sensor/snmp_sensor.cpp
+++ components/snmp_sensor/snmp_sensor.cpp
@@ -7,30 +7,22 @@ namespace snmp_sensor {
 static const char *TAG = "snmp_sensor";
 
 void SnmpSensor::setup() {
-  ESP_LOGI(TAG, "SNMP sensor setup complete");
-  // NIC zde NEINICIALIZUJEME !
+  ESP_LOGI(TAG, "Initializing SNMP client...");
+
+  // použití portu 50000, 161 necháme UPSce
+  if (!snmp_.begin(50000)) {
+    ESP_LOGE(TAG, "Failed to start SNMP client on port 50000!");
+  } else {
+    ESP_LOGI(TAG, "SNMP client ready on local port 50000");
+  }
 }
 
 void SnmpSensor::update() {
-
-  // ---- Lazy init – první update po WiFi ----
-  if (!snmp_initialized_) {
-    ESP_LOGI(TAG, "Initializing SNMP client on port 50000...");
-
-    if (!snmp_.begin(50000)) {
-      ESP_LOGE(TAG, "Failed to init SNMP on port 50000");
-      return;
-    }
-
-    snmp_initialized_ = true;
-    ESP_LOGI(TAG, "SNMP client ready.");
-  }
-
-  // ---- SNMP GET ----
   ESP_LOGD(TAG, "SNMP GET host=%s community=%s oid=%s",
            host_.c_str(), community_.c_str(), oid_.c_str());
 
   long value = 0;
+
   bool ok = snmp_.get(
       host_.c_str(),
       community_.c_str(),
