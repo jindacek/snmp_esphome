e27d63cf560bdb10de980ffee06ff2baf2b73877
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-21 18:59:16 +0100

    Update sensor.py

--- components/snmp_sensor/sensor.py
+++ components/snmp_sensor/sensor.py
@@ -1,40 +1,14 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import (
-    CONF_ID,
-    CONF_NAME,
-    CONF_UNIT_OF_MEASUREMENT,
-    CONF_UPDATE_INTERVAL,
-)
-
-from . import snmp_ns
-
-CONF_HOST = "host"
-CONF_COMMUNITY = "community"
-CONF_OID = "oid"
-
-SnmpSensor = snmp_ns.class_("SnmpSensor", sensor.Sensor, cg.PollingComponent)
-
-CONFIG_SCHEMA = (
-    sensor.sensor_schema()
-    .extend({
-        cv.GenerateID(): cv.declare_id(SnmpSensor),
-        cv.Required(CONF_HOST): cv.string,
-        cv.Required(CONF_COMMUNITY): cv.string,
-        cv.Required(CONF_OID): cv.string,
-    })
-    .extend(cv.polling_component_schema("10s"))
-)
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-
-    # základní vlastnosti senzoru (název, jednotky)
-    await sensor.register_sensor(var, config)
-    await cg.register_component(var, config)
-
-    cg.add(var.set_host(config[CONF_HOST]))
-    cg.add(var.set_community(config[CONF_COMMUNITY]))
-    cg.add(var.set_oid(config[CONF_OID]))
-
+# This file intentionally left as a backward-compatibility stub.
+# The component is now configured via top-level:
+#
+# snmp_sensor:
+#   host: ...
+#   community: ...
+#   battery_voltage:
+#     name: ...
+#
+# Do NOT use:
+# sensor:
+#   - platform: snmp_sensor
+#
+# If you need old single-OID mode, keep an old tag of the repo.
