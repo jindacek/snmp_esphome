eaa61e6128bf87cdafc37a3380b41a3283b88c9e
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-21 09:09:36 +0100

    Update snmp_sensor.cpp

--- components/snmp_sensor/snmp_sensor.cpp
+++ components/snmp_sensor/snmp_sensor.cpp
@@ -75,7 +75,7 @@ void SnmpSensor::update() {
   }
 
   // OID seznam
-  const char *oids_num[8] = {
+  const char *oids_num[9] = {
     "1.3.6.1.2.1.1.3.0",                       // 0 Runtime (TimeTicks)
     "1.3.6.1.4.1.318.1.1.1.2.2.1.0",           // 1 Battery capacity
     "1.3.6.1.4.1.318.1.1.1.2.2.2.0",           // 2 Battery temp
