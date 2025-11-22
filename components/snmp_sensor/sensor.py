737aeac18c8ca7c45dedc933950334aa09f9fc71
jindacek <105168152+jindacek@users.noreply.github.com>
2025-11-22 17:22:40 +0100

    Update sensor.py

--- components/snmp_sensor/sensor.py
+++ components/snmp_sensor/sensor.py
@@ -1,37 +1,36 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, text_sensor, binary_sensor
+
+from esphome.components import sensor, text_sensor
 from esphome.const import (
     CONF_ID,
-    CONF_UNIT_OF_MEASUREMENT,
-    CONF_ACCURACY_DECIMALS,
+    CONF_HOST,
     CONF_UPDATE_INTERVAL,
 )
 
 from . import snmp_ns
 
-AUTO_LOAD = ["sensor", "text_sensor", "binary_sensor"]
+SnmpSensor = snmp_ns.class_("SnmpSensor", cg.PollingComponent)
 
-CONF_HOST = "host"
 CONF_COMMUNITY = "community"
 
-# numeric keys
-CONF_RUNTIME = "runtime"
-CONF_BATTERY_CAPACITY = "battery_capacity"
-CONF_BATTERY_TEMP = "battery_temp"
+# ---- numeric keys ----
 CONF_BATTERY_VOLTAGE = "battery_voltage"
 CONF_INPUT_VOLTAGE = "input_voltage"
 CONF_OUTPUT_VOLTAGE = "output_voltage"
 CONF_LOAD = "load"
-CONF_OUTPUT_STATUS = "output_status"
+CONF_RUNTIME = "runtime"
 CONF_REMAINING_RUNTIME = "remaining_runtime"
-CONF_SELF_TEST_RESULT = "self_test_result"
-CONF_BATTERY_REPLACE_STATUS = "battery_replace_status"
-CONF_OUTPUT_SOURCE = "output_source"
+CONF_BATTERY_CAPACITY = "battery_capacity"
+CONF_BATTERY_TEMP = "battery_temp"
 CONF_INPUT_FREQUENCY = "input_frequency"
 CONF_OUTPUT_FREQUENCY = "output_frequency"
+CONF_OUTPUT_STATUS = "output_status"
+CONF_OUTPUT_SOURCE = "output_source"
+CONF_BATTERY_REPLACE_STATUS = "battery_replace_status"
+CONF_SELF_TEST_RESULT = "self_test_result"
 
-# text keys
+# ---- text keys ----
 CONF_MODEL = "model"
 CONF_UPS_NAME = "ups_name"
 CONF_MANUFACTURE_DATE = "manufacture_date"
@@ -41,21 +40,15 @@ CONF_SERIAL_NUMBER = "serial_number"
 CONF_OUTPUT_STATUS_TEXT = "output_status_text"
 CONF_RUNTIME_FORMATTED = "runtime_formatted"
 CONF_REMAINING_RUNTIME_FORMATTED = "remaining_runtime_formatted"
+CONF_OUTPUT_SOURCE_TEXT = "output_source_text"
+CONF_BATTERY_REPLACE_TEXT = "battery_replace_text"
+CONF_SELF_TEST_TEXT = "self_test_text"
 
-# binary keys (variant 4)
-CONF_ON_BATTERY = "on_battery"
-CONF_ONLINE = "online"
-
-SnmpSensor = snmp_ns.class_("SnmpSensor", cg.PollingComponent)
-
-NUM_SENSOR_SCHEMA = sensor.sensor_schema().extend(
-    {
-        cv.Optional(CONF_UNIT_OF_MEASUREMENT): cv.string,
-        cv.Optional(CONF_ACCURACY_DECIMALS): cv.int_,
-    }
+NUMERIC_SCHEMA = sensor.sensor_schema(
+    accuracy_decimals=0
 )
-TEXT_SENSOR_SCHEMA = text_sensor.text_sensor_schema()
-BINARY_SENSOR_SCHEMA = binary_sensor.binary_sensor_schema()
+
+TEXT_SCHEMA = text_sensor.text_sensor_schema()
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -63,39 +56,37 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Required(CONF_HOST): cv.string,
         cv.Required(CONF_COMMUNITY): cv.string,
 
-        # numeric
-        cv.Optional(CONF_RUNTIME): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_BATTERY_CAPACITY): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_BATTERY_TEMP): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_BATTERY_VOLTAGE): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_INPUT_VOLTAGE): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_OUTPUT_VOLTAGE): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_LOAD): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_OUTPUT_STATUS): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_REMAINING_RUNTIME): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_SELF_TEST_RESULT): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_BATTERY_REPLACE_STATUS): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_OUTPUT_SOURCE): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_INPUT_FREQUENCY): NUM_SENSOR_SCHEMA,
-        cv.Optional(CONF_OUTPUT_FREQUENCY): NUM_SENSOR_SCHEMA,
+        # ---- numeric optional ----
+        cv.Optional(CONF_BATTERY_VOLTAGE): NUMERIC_SCHEMA,
+        cv.Optional(CONF_INPUT_VOLTAGE): NUMERIC_SCHEMA,
+        cv.Optional(CONF_OUTPUT_VOLTAGE): NUMERIC_SCHEMA,
+        cv.Optional(CONF_LOAD): NUMERIC_SCHEMA,
+        cv.Optional(CONF_RUNTIME): NUMERIC_SCHEMA,
+        cv.Optional(CONF_REMAINING_RUNTIME): NUMERIC_SCHEMA,
+        cv.Optional(CONF_BATTERY_CAPACITY): NUMERIC_SCHEMA,
+        cv.Optional(CONF_BATTERY_TEMP): NUMERIC_SCHEMA,
+        cv.Optional(CONF_INPUT_FREQUENCY): NUMERIC_SCHEMA,
+        cv.Optional(CONF_OUTPUT_FREQUENCY): NUMERIC_SCHEMA,
+        cv.Optional(CONF_OUTPUT_STATUS): NUMERIC_SCHEMA,
+        cv.Optional(CONF_OUTPUT_SOURCE): NUMERIC_SCHEMA,
+        cv.Optional(CONF_BATTERY_REPLACE_STATUS): NUMERIC_SCHEMA,
+        cv.Optional(CONF_SELF_TEST_RESULT): NUMERIC_SCHEMA,
 
-        # text
-        cv.Optional(CONF_MODEL): TEXT_SENSOR_SCHEMA,
-        cv.Optional(CONF_UPS_NAME): TEXT_SENSOR_SCHEMA,
-        cv.Optional(CONF_MANUFACTURE_DATE): TEXT_SENSOR_SCHEMA,
-        cv.Optional(CONF_LAST_BATTERY_REPLACEMENT): TEXT_SENSOR_SCHEMA,
-        cv.Optional(CONF_LAST_SELF_TEST_DATE): TEXT_SENSOR_SCHEMA,
-        cv.Optional(CONF_SERIAL_NUMBER): TEXT_SENSOR_SCHEMA,
-        cv.Optional(CONF_OUTPUT_STATUS_TEXT): TEXT_SENSOR_SCHEMA,
-        cv.Optional(CONF_RUNTIME_FORMATTED): TEXT_SENSOR_SCHEMA,
-        cv.Optional(CONF_REMAINING_RUNTIME_FORMATTED): TEXT_SENSOR_SCHEMA,
-
-        # binary (odvozené)
-        cv.Optional(CONF_ON_BATTERY): BINARY_SENSOR_SCHEMA,
-        cv.Optional(CONF_ONLINE): BINARY_SENSOR_SCHEMA,
+        # ---- text optional ----
+        cv.Optional(CONF_MODEL): TEXT_SCHEMA,
+        cv.Optional(CONF_UPS_NAME): TEXT_SCHEMA,
+        cv.Optional(CONF_MANUFACTURE_DATE): TEXT_SCHEMA,
+        cv.Optional(CONF_LAST_BATTERY_REPLACEMENT): TEXT_SCHEMA,
+        cv.Optional(CONF_LAST_SELF_TEST_DATE): TEXT_SCHEMA,
+        cv.Optional(CONF_SERIAL_NUMBER): TEXT_SCHEMA,
+        cv.Optional(CONF_OUTPUT_STATUS_TEXT): TEXT_SCHEMA,
+        cv.Optional(CONF_RUNTIME_FORMATTED): TEXT_SCHEMA,
+        cv.Optional(CONF_REMAINING_RUNTIME_FORMATTED): TEXT_SCHEMA,
+        cv.Optional(CONF_OUTPUT_SOURCE_TEXT): TEXT_SCHEMA,
+        cv.Optional(CONF_BATTERY_REPLACE_TEXT): TEXT_SCHEMA,
+        cv.Optional(CONF_SELF_TEST_TEXT): TEXT_SCHEMA,
     }
 ).extend(cv.polling_component_schema("10s"))
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -104,48 +95,108 @@ async def to_code(config):
     cg.add(var.set_host(config[CONF_HOST]))
     cg.add(var.set_community(config[CONF_COMMUNITY]))
 
-    async def add_num(key, setter):
-        if key in config:
-            sens = await sensor.new_sensor(config[key])
-            cg.add(setter(sens))
-
-    async def add_txt(key, setter):
-        if key in config:
-            sens = await text_sensor.new_text_sensor(config[key])
-            cg.add(setter(sens))
-
-    async def add_bin(key, setter):
-        if key in config:
-            sens = await binary_sensor.new_binary_sensor(config[key])
-            cg.add(setter(sens))
-
     # numeric
-    await add_num(CONF_RUNTIME, var.set_runtime_sensor)
-    await add_num(CONF_BATTERY_CAPACITY, var.set_battery_capacity_sensor)
-    await add_num(CONF_BATTERY_TEMP, var.set_battery_temp_sensor)
-    await add_num(CONF_BATTERY_VOLTAGE, var.set_battery_voltage_sensor)
-    await add_num(CONF_INPUT_VOLTAGE, var.set_input_voltage_sensor)
-    await add_num(CONF_OUTPUT_VOLTAGE, var.set_output_voltage_sensor)
-    await add_num(CONF_LOAD, var.set_load_sensor)
-    await add_num(CONF_OUTPUT_STATUS, var.set_output_status_sensor)
-    await add_num(CONF_REMAINING_RUNTIME, var.set_remaining_runtime_sensor)
-    await add_num(CONF_SELF_TEST_RESULT, var.set_self_test_result_sensor)
-    await add_num(CONF_BATTERY_REPLACE_STATUS, var.set_battery_replace_status_sensor)
-    await add_num(CONF_OUTPUT_SOURCE, var.set_output_source_sensor)
-    await add_num(CONF_INPUT_FREQUENCY, var.set_input_frequency_sensor)
-    await add_num(CONF_OUTPUT_FREQUENCY, var.set_output_frequency_sensor)
+    if CONF_BATTERY_VOLTAGE in config:
+        s = await sensor.new_sensor(config[CONF_BATTERY_VOLTAGE])
+        cg.add(var.set_battery_voltage_sensor(s))
+
+    if CONF_INPUT_VOLTAGE in config:
+        s = await sensor.new_sensor(config[CONF_INPUT_VOLTAGE])
+        cg.add(var.set_input_voltage_sensor(s))
+
+    if CONF_OUTPUT_VOLTAGE in config:
+        s = await sensor.new_sensor(config[CONF_OUTPUT_VOLTAGE])
+        cg.add(var.set_output_voltage_sensor(s))
+
+    if CONF_LOAD in config:
+        s = await sensor.new_sensor(config[CONF_LOAD])
+        cg.add(var.set_load_sensor(s))
+
+    if CONF_RUNTIME in config:
+        s = await sensor.new_sensor(config[CONF_RUNTIME])
+        cg.add(var.set_runtime_sensor(s))
+
+    if CONF_REMAINING_RUNTIME in config:
+        s = await sensor.new_sensor(config[CONF_REMAINING_RUNTIME])
+        cg.add(var.set_remaining_runtime_sensor(s))
+
+    if CONF_BATTERY_CAPACITY in config:
+        s = await sensor.new_sensor(config[CONF_BATTERY_CAPACITY])
+        cg.add(var.set_battery_capacity_sensor(s))
+
+    if CONF_BATTERY_TEMP in config:
+        s = await sensor.new_sensor(config[CONF_BATTERY_TEMP])
+        cg.add(var.set_battery_temp_sensor(s))
+
+    if CONF_INPUT_FREQUENCY in config:
+        s = await sensor.new_sensor(config[CONF_INPUT_FREQUENCY])
+        cg.add(var.set_input_frequency_sensor(s))
+
+    if CONF_OUTPUT_FREQUENCY in config:
+        s = await sensor.new_sensor(config[CONF_OUTPUT_FREQUENCY])
+        cg.add(var.set_output_frequency_sensor(s))
+
+    if CONF_OUTPUT_STATUS in config:
+        s = await sensor.new_sensor(config[CONF_OUTPUT_STATUS])
+        cg.add(var.set_output_status_sensor(s))
+
+    if CONF_OUTPUT_SOURCE in config:
+        s = await sensor.new_sensor(config[CONF_OUTPUT_SOURCE])
+        cg.add(var.set_output_source_sensor(s))
+
+    if CONF_BATTERY_REPLACE_STATUS in config:
+        s = await sensor.new_sensor(config[CONF_BATTERY_REPLACE_STATUS])
+        cg.add(var.set_battery_replace_status_sensor(s))
+
+    if CONF_SELF_TEST_RESULT in config:
+        s = await sensor.new_sensor(config[CONF_SELF_TEST_RESULT])
+        cg.add(var.set_self_test_result_sensor(s))
 
     # text
-    await add_txt(CONF_MODEL, var.set_model_text_sensor)
-    await add_txt(CONF_UPS_NAME, var.set_ups_name_text_sensor)
-    await add_txt(CONF_MANUFACTURE_DATE, var.set_manufacture_date_text_sensor)
-    await add_txt(CONF_LAST_BATTERY_REPLACEMENT, var.set_last_battery_replacement_text_sensor)
-    await add_txt(CONF_LAST_SELF_TEST_DATE, var.set_last_self_test_text_sensor)
-    await add_txt(CONF_SERIAL_NUMBER, var.set_serial_number_text_sensor)
-    await add_txt(CONF_OUTPUT_STATUS_TEXT, var.set_output_status_text_sensor)
-    await add_txt(CONF_RUNTIME_FORMATTED, var.set_runtime_formatted_text_sensor)
-    await add_txt(CONF_REMAINING_RUNTIME_FORMATTED, var.set_remaining_runtime_formatted_text_sensor)
+    if CONF_MODEL in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_MODEL])
+        cg.add(var.set_model_text_sensor(ts))
 
-    # binary
-    await add_bin(CONF_ON_BATTERY, var.set_on_battery_binary_sensor)
-    await add_bin(CONF_ONLINE, var.set_online_binary_sensor)
+    if CONF_UPS_NAME in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_UPS_NAME])
+        cg.add(var.set_name_text_sensor(ts))
+
+    if CONF_MANUFACTURE_DATE in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_MANUFACTURE_DATE])
+        cg.add(var.set_manufacture_date_text_sensor(ts))
+
+    if CONF_LAST_BATTERY_REPLACEMENT in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_LAST_BATTERY_REPLACEMENT])
+        cg.add(var.set_last_battery_replacement_text_sensor(ts))
+
+    if CONF_LAST_SELF_TEST_DATE in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_LAST_SELF_TEST_DATE])
+        cg.add(var.set_last_self_test_text_sensor(ts))
+
+    if CONF_SERIAL_NUMBER in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_SERIAL_NUMBER])
+        cg.add(var.set_serial_number_text_sensor(ts))
+
+    if CONF_OUTPUT_STATUS_TEXT in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_OUTPUT_STATUS_TEXT])
+        cg.add(var.set_output_status_text_sensor(ts))
+
+    if CONF_RUNTIME_FORMATTED in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_RUNTIME_FORMATTED])
+        cg.add(var.set_runtime_formatted_text_sensor(ts))
+
+    if CONF_REMAINING_RUNTIME_FORMATTED in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_REMAINING_RUNTIME_FORMATTED])
+        cg.add(var.set_remaining_runtime_formatted_text_sensor(ts))
+
+    if CONF_OUTPUT_SOURCE_TEXT in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_OUTPUT_SOURCE_TEXT])
+        cg.add(var.set_output_source_text_sensor(ts))
+
+    if CONF_BATTERY_REPLACE_TEXT in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_BATTERY_REPLACE_TEXT])
+        cg.add(var.set_battery_replace_text_sensor(ts))
+
+    if CONF_SELF_TEST_TEXT in config:
+        ts = await text_sensor.new_text_sensor(config[CONF_SELF_TEST_TEXT])
+        cg.add(var.set_self_test_text_sensor(ts))
